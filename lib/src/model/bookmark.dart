69ac4c1b123e475e4322245ba53082a0c072232f
Victor <victor.catamo.rojas@gmail.com>
2020-03-06 00:18:16 +0100

    Initial Commit - Progress on DartAngular Bookmarks

---
+++ lib/src/model/bookmark.dart
@@ -0,0 +1,19 @@
+class Bookmark {
+  Bookmark(
+      {this.title = '',
+      this.description = '',
+      this.url = '',
+      this.edit = true});
+
+  update(Bookmark bm) {
+    title = bm.title;
+    description = bm.description;
+    url = bm.url;
+    edit = bm.edit;
+  }
+
+  String title;
+  String description;
+  String url;
+  bool edit;
+}
